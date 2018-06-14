6b1ca94cccdbcf4b595f68dddc134dbdb514352c
Cameron Conn <cam@camconn.cc>
2018-06-13 21:28:32 -0500

    moving to gitlab

--- hidmsr/__init__.py
+++
@@ -1,8 +0,0 @@
-""" hidmsr - a library for interacting with the MSR605X magnetic card reader/writer.
-
-Licensed under the GNU Public License, version 3 or later.
-
-Copyright (c) 2018 Cameron Conn
-"""
-
-__all__ = ["convert", "commands"]
